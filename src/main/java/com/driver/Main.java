e5ec93d4c03d00de7ff0698be721163396eaf525
vadde suresh <sureshvadde1076@gmail.com>
2022-12-11 21:45:56 +0530

    lets chech

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Main {
     public static class Product {
         public int product(int x, int y) {
-            return x+y;
+            return x*y;
         }
         public int product(int x, int y, int z) {
-            return x+y+z;
+            return x*y*z;
         }
         public double product(double x, double y) {
-            return x+y;
+            return x*y;
         }
     }
    public static void main(String[] args){
